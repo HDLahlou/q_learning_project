a278c94d40313634f40796a555a76ec17c05c015
Hakim Dionisio Lahlou <hdlahlou@graveler.cs.uchicago.edu>
2021-02-28 14:48:57 -0600

    base color perception down

--- scripts/robot_perception.py
+++ scripts/robot_perception.py
@@ -1,90 +1,118 @@
 #!/usr/bin/env python3
 
-import rospy, cv2, cv_bridge, numpy
+import rospy, cv2, numpy
+from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+# import keras_ocr
+
 
 class Perception:
 
-        def __init__(self):
+    def __init__(self):
+        # set up ROS / OpenCV bridge
+        self.bridge = CvBridge()
 
-                # set up ROS / OpenCV bridge
-                self.bridge = cv_bridge.CvBridge()
+        lower_red = numpy.array([0, 100, 100])
+        upper_red = numpy.array([10, 255, 255])
+        self.red_ranges = [lower_red, upper_red]
 
-                # initalize the debugging window
-                cv2.namedWindow("window", 1)
+        lower_blue = numpy.array([110, 100, 100])
+        upper_blue = numpy.array([130, 255, 255])
+        self.blue_ranges = [lower_blue, upper_blue]
 
-                # subscribe to the robot's RGB camera data stream
-                self.image_sub = rospy.Subscriber('camera/rgb/image_raw',
-                        Image, self.image_callback)
+        lower_green = numpy.array([50, 100, 100])
+        upper_green = numpy.array([70, 255, 255])
+        self.green_ranges = [lower_green, upper_green]
 
-                self.velocity_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self.searching_for_color = True
+        # subscribe to the robot's RGB camera data stream
+        self.image_sub = rospy.Subscriber('camera/rgb/image_raw',
+                Image, self.image_callback)
 
-        def image_callback(self, msg):
+        self.velocity_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        # download pre-trained model
+        # self.pipeline = keras_ocr.pipeline.Pipeline()
 
-                # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
-                image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-                hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # def char_image_callback(self, img):
+        # Once you have the pipeline, you can use it to recognize characters,
 
-                # TODO: define the upper and lower bounds for what should be considered 'yellow'
-                lower_red = numpy.array([0, 100, 100])
-                upper_red = numpy.array([10, 255, 255])
+        # images is a list of images in the cv2 format
+        # images = [img1, img2, ...]
 
-                lower_blue = numpy.array([110, 100, 100])
-                upper_blue = numpy.array([130, 255, 255])
+        # call the recognizer on the list of images
+        # prediction_groups = pipline.recognize(images)
 
-                lower_green = numpy.array([50, 100, 100])
-                upper_green = numpy.array([70, 255, 255])
-                mask = cv2.inRange(hsv, lower_red, upper_red)
+        # prediction_groups is a list of predictions for each image
+        # prediction_groups[0] is a list of tuples for recognized characters for img1
+        # the tuples are of the formate (word, box), where word is the word
+        # recognized by the recognizer and box is a rectangle in the image where the recognized words reside
 
-                # this erases all pixels that aren't yellow
-                h, w, d = image.shape
-                search_top = int(3*h/4)
-                search_bot = int(3*h/4 + 20)
-                mask[0:search_top, 0:w] = 0
-                mask[search_bot:h, 0:w] = 0
+    def image_callback(self, msg):
+        if self.searching_for_color:
+            # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
+            image = self.bridge.imgmsg_to_cv2(msg,'bgr8')
 
-                # using moments() function, the center of the yellow pixels is determined
-                M = cv2.moments(mask)
-                # if there are any yellow pixels found
-                if M['m00'] > 0:
-                        # center of the yellow pixels in the image
-                        cx = int(M['m10']/M['m00'])
-                        cy = int(M['m01']/M['m00'])
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            input = "green"
+            if input == "red":
+                color_range = self.red_ranges
+            elif input == "blue":
+                color_range = self.blue_ranges
+            elif input == "green":
+                color_range = self.green_ranges
 
-                        # a red circle is visualized in the debugging window to indicate
-                        # the center point of the yellow pixels
-                        cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
 
-                        # TODO: based on the location of the line (approximated
-                        #       by the center of the yellow pixels), implement
-                        #       proportional control to have the robot follow
-                        #       the yellow line
-                        # Constants
-                        xx = w/2
-                        yy = 370
-                        vel_msg = Twist()
+            mask = cv2.inRange(hsv,color_range[0], color_range[1])
+
+            h, w, d = image.shape
+
+            # using moments() function, the center of the yellow pixels is determined
+            M = cv2.moments(mask)
+            # print(M)
+            # if there are any yellow pixels found
+            if M['m00'] > 0:
+                    # center of the yellow pixels in the image
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+
+                    # a red circle is visualized in the debugging window to indicate
+                    # the center point of the yellow pixels
+                    # cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+
+                    # TODO: based on the location of the line (approximated
+                    #       by the center of the yellow pixels), implement
+                    #       proportional control to have the robot follow
+                    #       the yellow line
+                    # Constants
+                    xx = w/2
+                    yy = 370
+                    vel_msg = Twist()
+                    if cy < 280:
+                        vel_msg.linear.x = .1
+                    else:
                         vel_msg.linear.x = 0
-                        print(h,w,d)
-                        print("x: " + str(cx) + "y: " + str(cy))
-                        kp = .01
-                        # Calc of offset on center of yellow
-                        et = xx - cx
-                        # Calculation to center
-                        pc = kp*et
-                        vel_msg.angular.z = pc
-                        self.velocity_pub.publish(vel_msg)
 
-                # shows the debugging window
-                cv2.imshow("window", image)
-                cv2.waitKey(3)
+                    print("x: " + str(cx) + "y: " + str(cy))
+                    kp = .01
+                    # Calc of offset on center of yellow
+                    et = xx - cx
+                    # Calculation to center
+                    pc = kp*et
+                    vel_msg.angular.z = pc
+                    self.velocity_pub.publish(vel_msg)
+            else:
+                vel_msg = Twist()
+                vel_msg.linear.x = 0
+                vel_msg.angular.z = .1
+                self.velocity_pub.publish(vel_msg)
+        
 
 
-        def run(self):
-                rospy.spin()
-                
+    def run(self):
+        rospy.spin()
+            
 if __name__ == '__main__':
-
-        rospy.init_node('line_follower')
-        follower = Follower()
-        follower.run()
+    rospy.init_node('Perception')
+    follower = Perception()
+    follower.run()
